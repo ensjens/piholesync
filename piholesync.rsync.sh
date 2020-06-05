1906e9cc0ec6e1f64437e6f354f020762d557fc5
ensjens <64878876+ensjens@users.noreply.github.com>
2020-06-05 14:24:08 -0400

    Update piholesync.rsync.sh

--- piholesync.rsync.sh
+++ piholesync.rsync.sh
@@ -26,14 +26,14 @@ FILECHANGEREL=`echo "$FILECHANGE" | sed 's_'$CURPATH'/__'`
 FILES=(gravity.db custom.list) #list of files you want to sync
 PIHOLEDIR=/etc/pihole #working dir of pihole
 PIHOLE2=172.16.10.4 #IP of 2nd PiHole
-HAUSER=root #user of second pihole
+HAUSER=pi #user of second pihole
 
 #LOOP FOR FILE TRANSFER
 RESTART=0 # flag determine if service restart is needed
 for FILE in ${FILES[@]}
 do
   if [[ -f $PIHOLEDIR/$FILE ]]; then
-  RSYNC_COMMAND=$(rsync -aiu $PIHOLEDIR/$FILE $HAUSER@$PIHOLE2:$PIHOLEDIR)
+  RSYNC_COMMAND=$(rsync -aiuv $PIHOLEDIR/$FILE $HAUSER@$PIHOLE2:$PIHOLEDIR)
     if [[ -n "${RSYNC_COMMAND}" ]]; then
       # rsync copied changes
       RESTART=1 # restart flagged
@@ -46,7 +46,7 @@ do
 done
 
 FILE="adlists.list"
-RSYNC_COMMAND=$(rsync -aiu $PIHOLEDIR/$FILE $HAUSER@$PIHOLE2:$PIHOLEDIR)
+RSYNC_COMMAND=$(rsync -aiuv $PIHOLEDIR/$FILE $HAUSER@$PIHOLE2:$PIHOLEDIR)
 if [[ -n "${RSYNC_COMMAND}" ]]; then
   # rsync copied changes, update GRAVITY
   ssh $HAUSER@$PIHOLE2 "sudo -S pihole -g"
